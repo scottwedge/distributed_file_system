3f75e01b4618a892ee9c8d7f7edccdfa367a47eb
KareemOmar <kareemomar96@hotmail.com>
2020-03-14 20:38:06 +0200

    replication functions implemented
    - fixed simple errors
    - implemented functions
    * needs testing
    * still needs connection fixing

--- master/replication.py
+++ master/replication.py
@@ -1,6 +1,7 @@
 # TODO : complete file to send periodic replication signal
 import time
 import zmq
+import sys
 
 def replication_func(availability_table,file_names_tables):
     port_num =7000 #back door for replication
@@ -11,6 +12,7 @@ def replication_func(availability_table,file_names_tables):
     maxNumOfReplications=min(3,data_keepers_num) #less than 3 if we have less than 3 data keepers
     context = zmq.Context()
     ## TODO: fix connection , you can't bind same ip & port multiple times
+    # why? we can't use the existing ports for upload and download?
     sockets={}
     for i in range(data_keepers_num):
         print ("binding replication port of datakeeper no. "+str(i+1))
@@ -24,27 +26,53 @@ def replication_func(availability_table,file_names_tables):
         for item in file_names_tables.items():
             IP,files = item
             for file in files:
-                if(Need_to_replicate(file)):
+                if(Need_to_replicate(file,file_names_tables,maxNumOfReplications)):
                     #get another IP other than found
                     nextIP=getNextOtherIP(IP,file_names_tables,file)
                     #get free port to send to
                     nextPort=getFirstNextFreePort(nextIP,availability_table)
                     availability_table[nextPort] = False
-                    print(file_name + "is taken now for replication..")
+                    print(file + "is taken now for replication..")
                     sockets[IP].send_pyobj([nextPort,file]) #send to the datakeeper having the file to send it to other data keeper given
-                    wait(1)
+                    time.sleep(1)
 
 
 #returns true if file does Need_to_replicate exist in tables<maxNumOfReplications
-def Need_to_replicate(file_name):
-    pass
-    ## TODO: implement
+def Need_to_replicate(file_name,file_names_tables,maxNumOfReplications):
+    temp_counter = 0
+    for item in file_names_tables.items():
+        IP,files = item
+        for file in files:
+            if(file_name == file):
+                temp_counter+=1
+    if (temp_counter >= maxNumOfReplications): # should be == but used >= to avoid potential issues
+        return False
+    else:
+        return True
+    ## Done: implement
+    ## TODO: test
 
 #get ip doesn't have the same file
-def getNextOtherIP(IP,file_names_tables,file):
-    pass
-    ## TODO: implement
+def getNextOtherIP(IP,file_names_tables,file_name):
+    for item in file_names_tables.items():
+        file_exists_in_current_data_keeper = False
+        IP,files = item
+        for file in files:
+            if(file_name == file):
+                file_exists_in_current_data_keeper = True
+        if (file_exists_in_current_data_keeper == False):
+            return IP
+    return "-1" # all IPs have this file
+    
+    ## Done: implement
+    ## TODO: test
 
 def getFirstNextFreePort(nextIP,availability_table):
-    pass
-    ## TODO: implement
+    for item in availability_table.items():
+        IPport,status = item
+        IP,port=IPport.split(":")
+        if (IP == nextIP and status == True):
+            return IPport
+    return "-1"
+    ## Done: implement
+    ## TODO: test
