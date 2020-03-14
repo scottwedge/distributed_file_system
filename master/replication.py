625e3c8875d2f7d1fe85b450c0f2797ad2b07959
KareemOmar <kareemomar96@hotmail.com>
2020-03-14 21:58:16 +0200

    replication connection fix attempt#1
    added a potential solution for the replication connection issue

--- master/replication.py
+++ master/replication.py
@@ -4,16 +4,15 @@ import zmq
 import sys
 
 def replication_func(availability_table,file_names_tables):
-    port_num =7000 #back door for replication
+    replication_port =7000 #back door for replication
     data_keepers_num = -1
     if (len(sys.argv) > 1):
         data_keepers_num = int(sys.argv[1])
     print("identifying "+str(data_keepers_num)+" data keepers in the replication system")
     maxNumOfReplications=min(3,data_keepers_num) #less than 3 if we have less than 3 data keepers
     context = zmq.Context()
-    ## TODO: fix connection , you can't bind same ip & port multiple times
-    # why? we can't use the existing ports for upload and download?
-    sockets={}
+    replication_socket = context.socket(zmq.REQ)
+    #sockets={}
     for i in range(data_keepers_num):
         print ("binding replication port of datakeeper no. "+str(i+1))
         sockets[ip] = context.socket(zmq.Push)
@@ -27,13 +26,17 @@ def replication_func(availability_table,file_names_tables):
             IP,files = item
             for file in files:
                 if(Need_to_replicate(file,file_names_tables,maxNumOfReplications)):
+                    replication_socket.connect("tcp://%s:%s"  %( IP, replication_port))
                     #get another IP other than found
                     nextIP=getNextOtherIP(IP,file_names_tables,file)
                     #get free port to send to
                     nextPort=getFirstNextFreePort(nextIP,availability_table)
                     availability_table[nextPort] = False
                     print(file + "is taken now for replication..")
-                    sockets[IP].send_pyobj([nextPort,file]) #send to the datakeeper having the file to send it to other data keeper given
+                    # sockets[IP].send_pyobj([nextPort,file]) #send to the datakeeper having the file to send it to other data keeper given
+                    replication_socket.send_pyobj([nextPort,file])
+                    replication_socket.recv_pyobj()
+                    replication_socket.close() # socket.disconnect() may work
                     time.sleep(1)
 
 
